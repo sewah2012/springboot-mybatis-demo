34a929d68a71eb5b99507348d646f8cc5bcc593f
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2022-11-24 16:33:19 +0100

    bootstrap and initialize project

---
+++ src/main/java/io/sewah/spingbootmybatisdemo/SpingbootMybatisDemoApplication.java
@@ -0,0 +1,13 @@
+package io.sewah.spingbootmybatisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootMybatisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootMybatisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sewah/spingbootmybatisdemo/SpingbootMybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sewah.spingbootmybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootMybatisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
