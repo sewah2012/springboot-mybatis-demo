de244b157e3cc385f2fefa68af79f3f591f9241a
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2022-11-28 12:05:55 +0100

    setup db schema and add h2 to classpath and configure xml mapper

--- src/main/java/io/sewah/spingbootmybatisdemo/mybatis/mappers/ProductMapper.java
+++ src/main/java/io/sewah/spingbootmybatisdemo/mybatis/mappers/ProductMapper.java
@@ -1,10 +1,12 @@
 package io.sewah.spingbootmybatisdemo.mybatis.mappers;
 
 import io.sewah.spingbootmybatisdemo.mybatis.models.Product;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 public interface ProductMapper {
     public List<Product> getAllProduct();
     public void addProduct(Product product);
